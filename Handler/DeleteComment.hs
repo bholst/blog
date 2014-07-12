a84f2bfae79f9308ff987e1ed26207f626b5fc2a
Bastian Holst <bastianholst@gmx.de>
2014-07-12 16:33:03 +0200

    Finally Added deleteComment

---
+++ Handler/DeleteComment.hs
@@ -0,0 +1,35 @@
+module Handler.DeleteComment
+  ( getDeleteCommentR
+  , postDeleteCommentR )
+where
+
+import Import
+
+deleteForm :: Html -> MForm Handler (FormResult (), Widget)
+deleteForm extra = do
+  (submitRes, submitView) <- mbootstrapSubmit (BootstrapSubmit MsgReallyDeleteSubmit "" [])
+  let widget = $(widgetFile "delete-comment-form")
+  return (submitRes, widget)
+
+getDeleteCommentR :: CommentId -> Handler Html
+getDeleteCommentR commentId = do
+  comment <- runDB $ get404 commentId
+  (formWidget, enctype) <- generateFormPost deleteForm
+  defaultLayout $ do
+    setTitleI MsgReallyDeleteCommentTitle
+    $(widgetFile "delete-comment")
+
+postDeleteCommentR :: CommentId -> Handler Html
+postDeleteCommentR commentId = do
+  ((res, _), _) <-
+    runFormPost deleteForm
+  comment <- runDB $ get404 commentId
+  case res of
+    FormSuccess _ -> do
+      runDB $ delete commentId
+      setMessageI MsgCommentDeleted
+      redirect $ EntryR (commentEntry comment)
+    _ -> do
+      setMessageI MsgCommentNotDeleted
+      redirect $ EntryR (commentEntry comment)
+
